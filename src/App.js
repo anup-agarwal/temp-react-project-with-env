06c361602a895d02de3fb640e6482f606fa71415
Anup Agarwal <anupagarwal@Anups-MacBook-Pro.local>
2021-10-14 01:55:01 +0530

    using env variable

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {process.env.REACT_APP_ENV_VAR}
         </a>
       </header>
     </div>
